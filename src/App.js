4b7cbde454ac1c5cf2862885d6ede1ccee8a5412
OscarGO14 <ogo53140@gmail.com>
2020-11-06 16:55:37 +0100

    vuelta al background limpio

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import About from './components/About';
@@ -6,40 +6,40 @@ import Resume from './components/Resume';
 import Projects from './components/Projects';
 import Contacto from './components/Contacto';
 import Footer from './components/Footer';
-import { Home } from './Home';
+//import { Home } from './Home';
 import { Zoom } from 'react-reveal'
 
 function App() {
 
   // Landing
-  const [cohete, setCohete] = useState('fly')
-  const [condition, setCondition] = useState(null)
-
-  const loadPage = () => {
-    setCohete('dimensiones')
-    setTimeout(() => setCondition(true), 3000)
-  }
-
+  /* const [cohete, setCohete] = useState('fly')
+   const [condition, setCondition] = useState(null)
+ 
+   const loadPage = () => {
+     setCohete('dimensiones')
+     setTimeout(() => setCondition(true), 3000)
+   }
+ */
   return (
-    <div onClick={loadPage} className="app">
-      <div className="landingPage">
-        {!condition && <h1 style={{ position: 'absolute', top: '2vh' }} >Portfolio de OscarGO</h1>}
+    <div className="app">
+      {/*<div className="landingPage">
+      }  {!condition && <h1 style={{ position: 'absolute', top: '2vh' }} >Portfolio de OscarGO</h1>}
         <Home cohete={cohete} />
         {!condition && <h1>Haz click para ver "Landing Page"</h1>}
-        {condition && <h1> </h1>}
+      {condition && <h1> </h1>}
       </div>
-      { condition &&
-        <div>
-          <Zoom>
-            <Header />
-          </Zoom>
-          <About />
-          <Resume />
-          <Projects />
-          <Contacto />
-          <Footer />
-        </div>
-      }
+      { condition &&*/}
+      <div>
+        <Zoom>
+          <Header />
+        </Zoom>
+        <About />
+        <Resume />
+        <Projects />
+        <Contacto />
+        <Footer />
+      </div>
+
 
 
 
